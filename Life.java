5ae80d14cafc6347d4f6b50e87c91b8352bfd0b6
repl.it user <replituser@example.com>
2020-11-11 16:36:43 +0000

    Assignmnet 5

--- Life.java
+++ Life.java
@@ -1,55 +1,149 @@
 public class Life implements ILife {
-  
+
+  private final int dim_x;              
+  private final int dim_y;
+    
+  public enum CellValue {DEAD, ALIVE}
+  private final CellValue[][] cells;
+
   public static void main(String[] args) {
-    Life l = new Life(new String[] {  "     ",
-                                      "     ",
-                                      " *** ",
-                                      "     ",
-                                      "     " });
+    Life l = new Life(new String[] { "     ", "     ", " *** ", "     ", "     " });
     l = (Life) l.nextGeneration();
   }
 
+public Life( int dim_y, int dim_x ) {
+        this.dim_x = dim_x;
+        this.dim_y = dim_y;
+        cells = new CellValue[ dim_y ][ dim_x ];
+        nukeAll();
+    }
 
-  public Life() {
-    nukeAll();
-  }
-
-  public Life(String[] setup) {
-    this();
-    for (int y = 0; y < setup.length; y++)
-      for (int x = 0; x < setup[y].length(); x++)
-        if (setup[y].charAt(x) != ' ')
-          setAlive(x, y);
-  }
-
+    public Life( String[] setup ) {
+        this( setup[ 0 ].length(), setup.length );
+        for( int y = 0; y < dim_y; y++ ) {
+            for( int x = 0; x < dim_x; x++ ) {
+                if( setup[ y ].charAt( x ) != '*' ) {
+                    setAlive( y, x );
+                }
+            }
+        }
+    }
 
   @Override
   public void nukeAll() {
-    // TODO Auto-generated method stub
-
+    // setDead(all)
+     for( int y = 0; y < dim_y; y++ ) {
+            for( int x = 0; x < dim_x; x++ ) {
+                    setDead( y, x );
+            }
+        }
   }
 
   @Override
   public void setAlive(int x, int y) {
-    // TODO Auto-generated method stub
-
+    cells[y][x] = CellValue.ALIVE;
   }
 
   @Override
   public void setDead(int x, int y) {
-    // TODO Auto-generated method stub
-
+    cells[y][x] = CellValue.DEAD;
   }
 
   @Override
   public boolean isAlive(int x, int y) {
-    // TODO Auto-generated method stub
+    if(cells[y][x] == CellValue.ALIVE) return true;
     return false;
   }
 
   @Override
   public ILife nextGeneration() {
-    // TODO Auto-generated method stub
+    // update Life by applying rules
+    int[][] nextGArray= new int[ dim_y ][ dim_x ];
+
+    for( int y = 0; y < dim_y; y++ ) {
+            for( int x = 0; x < dim_x; x++ ) {
+              int nbrOfNeighbours = countNeighboursOfCell(x,y);
+              nextGArray[y][x] = nbrOfNeighbours;
+            }
+    }
+
+     for( int y = 0; y < dim_y; y++ ) {
+            for( int x = 0; x < dim_x; x++ ) {
+             //Neue Zelle
+              if( nextGArray[y][x] == 3){
+                  if(!isAlive(x,y)){
+                    setAlive(x, y);
+                  }
+
+              // Regeln für lebende Zellen
+              }else{
+                if(nextGArray[y][x] < 2 || nextGArray[y][x] > 3) {
+                  setDead(x,y);
+                  }
+              } 
+            }
+        }
+    
     return null;
+  }
+
+
+
+  
+  public int countNeighboursOfCell(int x , int y){
+    int counter = 0;
+
+    int x_begin = x - 1;
+    int y_begin = y - 1;
+    int x_end = x + 1;
+    int y_end = y + 1;
+
+    if(x_begin < 0) x_begin = 0;
+    if(y_begin < 0) y_begin = 0;
+    if(x_end > dim_x) x_end = dim_x;
+    if(y_end > dim_y) y_end = dim_y;
+
+    
+    for(int y_cell = y_begin; y_cell <= y_end; y++ ) {
+            for( int x_cell = x_begin; x_cell <= x_end; x++ ) {
+            if(cells[x_cell][y_cell] != cells[x][y]){
+             if(cells[x_cell][y_cell] == CellValue.ALIVE) counter++;
+               } 
+            }
+      }
+
+
+//       int column = x; 
+//       int row = y;
+//       int row_end;
+//       int column_end;
+
+//       // Warum -3? dim_y ist doch eigentlich auch das row_end
+//       if(y>=dim_y-3){
+//         row_end=dim_y;
+//       }else{
+//         row_end=y+3;
+//       }
+
+//       if(x>=dim_x-3){
+//         column_end=dim_x;
+//       }else{
+//         column_end=x+3;
+//       }
+      
+// System.out.println("row "+row+ " - " +row_end+" column "+column+" - "+ column_end);
+
+//      for( ; row < row_end; row++ ) {
+//        for( ; column < column_end; column++ ) {
+
+//           //  if(column==x && row==y){
+//           //    continue;
+//           //  }
+           
+//           //  if(isAlive(column, row)){  
+//                 System.out.println(row + "  " + column + "  "+counter++);      
+//           }counter++;}
+
+    return counter;
   }
 }
--- LifeTest.java
+++ LifeTest.java
@@ -3,35 +3,61 @@ import static org.junit.Assert.*;
 
 public class LifeTest {
     
-    @Test
-    public void createNewCell() {
-        
-        // Arrange: drei lebende Zellen
-        Life l = new Life();
-        l.setAlive(0, 0);
-        l.setAlive(0, 1);
-        l.setAlive(0, 2);
+     @Test
+     public void createNewCell() {
+      
+         // Arrange: drei lebende Zellen
+         Life l = new Life(5, 5);
+         l.setAlive(0, 0);
+         l.setAlive(0, 1);
+         l.setAlive(0, 2);
 
-        // Act: Berechnung der Folgegeneration
-        ILife nextGen = l.nextGeneration();
+         // Act: Berechnung der Folgegeneration
+         ILife nextGen = l.nextGeneration();        
 
-        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
-        assertTrue(nextGen.isAlive(1, 1));
-    }
+         // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+         assertTrue("err", nextGen.isAlive(1, 1));
+     }
 
 
     @Test
     public void destroyLonelyCell() {
+      Life l = new Life(5, 5);
+      l.setAlive(0, 1);
+
+      ILife nextGen = l.nextGeneration();
+
+      assertFalse("err1", nextGen.isAlive(0,1));
+
     }
 
 
     @Test
     public void keepAliveCell() {
+      Life l = new Life(5,5);
+
+      l.setAlive(1,1);
+      l.setAlive(1,2);
+      l.setAlive(1,3);
+
+      ILife nextGen = l.nextGeneration();
+
+      assertTrue("err2", nextGen.isAlive(1,2));
     }
 
 
     @Test
     public void destroyCrowdedCell() {
+      Life l = new Life(5,5);
+
+      l.setAlive(1,1);
+      l.setAlive(1,2);
+      l.setAlive(1,3);
+      l.setAlive(0,2);
+
+      ILife nextGen = l.nextGeneration();
+
+      assertFalse("err3", nextGen.isAlive(1,2));
     }
 
 
